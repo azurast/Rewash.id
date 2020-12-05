8c9075a7dbbb6705c2b7f0d84e8af8c284b1f5c6
cyntiafanny <cyntia.fanny@student.umn.ac.id>
2020-12-06 03:51:16 +0700

    - implementation location input from user - calculate distance of eact outlet with position inputed - show outlet's review as a dropdown component

--- src/app/outlet/outlet.page.ts
+++ src/app/outlet/outlet.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { Outlet } from '../services/outlets/outlet.model';
-import { OutletService } from '../services/outlets/outlet.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {Outlet} from '../services/outlets/outlet.model';
+import {OutletService} from '../services/outlets/outlet.service';
+import {GooglePlaceDirective} from "ngx-google-places-autocomplete";
+import {Address} from "ngx-google-places-autocomplete/objects/address";
 
 @Component({
   selector: 'app-outlet',
@@ -9,14 +11,29 @@ import { OutletService } from '../services/outlets/outlet.service';
   styleUrls: ['./outlet.page.scss'],
 })
 export class OutletPage implements OnInit {
+  @ViewChild("placesRef") placesRef: GooglePlaceDirective;
   outlet: Outlet[] = [];
-  dummyLatitude = -6.255085;
-  dummyLongitude = 106.615671;
+  inputLatitude = 0;
+  inputLongitude = 0;
+  options = {
+    componentRestriction: {
+      country: ['ID']
+    }
+  }
 
   constructor(
     private outletService: OutletService,
     private db: AngularFireDatabase
   ) {
+  }
+
+  public handleAddressChange(address: Address) {
+    this.inputLatitude = address.geometry.location.lat();
+    this.inputLongitude = address.geometry.location.lng();
+
+    this.fetchOutletFromDatabase();
+    setTimeout(() => this.outlet.sort((a, b) => (a.distance > b.distance) ? 1 : -1), 2000);
+    this.outletService.storeOutlet(this.outlet);
   }
 
   getDistance(lat1, lon1, lat2, lon2) {
@@ -37,38 +54,40 @@ export class OutletPage implements OnInit {
   }
 
   fetchOutletFromDatabase() {
+    this.outlet = [];
     this.db.object('/outlet').query.once('value')
       .then(data => {
-          Object.keys(data.val()).forEach(outletKey => {
-              this.outlet.push({
-                id: outletKey,
-                feedbacks: data.val()[outletKey].feedbacks || [],
-                name: data.val()[outletKey].name,
-                location: data.val()[outletKey].location,
-                longitude: data.val()[outletKey].longitude,
-                latitude: data.val()[outletKey].latitude,
-                openHours: data.val()[outletKey].open_hours,
-                rate: data.val()[outletKey].rate,
-                transactions: data.val()[outletKey].transactions,
-                points: data.val()[outletKey].points,
-                admin: data.val()[outletKey].admin,
-                distance: this.getDistance(this.dummyLatitude, this.dummyLongitude,
-                  data.val()[outletKey].latitude, data.val()[outletKey].longitude
-                )
-              });
-            });
+        Object.keys(data.val()).forEach(outletKey => {
+          this.outlet.push({
+            id: outletKey,
+            feedbacks: data.val()[outletKey].feedbacks || [],
+            name: data.val()[outletKey].name,
+            location: data.val()[outletKey].location,
+            longitude: data.val()[outletKey].longitude,
+            latitude: data.val()[outletKey].latitude,
+            openHours: data.val()[outletKey].open_hours,
+            rate: data.val()[outletKey].rate,
+            transactions: data.val()[outletKey].transactions,
+            points: data.val()[outletKey].points,
+            admin: data.val()[outletKey].admin,
+            distance: this.getDistance(this.inputLatitude, this.inputLongitude,
+              data.val()[outletKey].latitude, data.val()[outletKey].longitude
+            )
+          });
+        });
       });
   }
 
   ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: Position) => {
+        this.inputLatitude = position.coords.latitude
+        this.inputLongitude = position.coords.longitude
+      })
+    }
+
     this.fetchOutletFromDatabase();
     setTimeout(() => this.outlet.sort((a, b) => (a.distance > b.distance) ? 1 : -1), 2000);
     this.outletService.storeOutlet(this.outlet);
   }
-
-  ionViewWillEnter() {
-    // this.outlet = this.outletService.getAllOutlets();
-  }
-
-
 }
